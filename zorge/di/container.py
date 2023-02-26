73fac878474c41b3fe2a1465e3433de0f405921f
smairon <man@smairon.ru>
2023-02-26 17:53:11 +0300

    Architecture refactoring

--- zorge/di/container.py
+++ zorge/di/container.py
@@ -1,168 +1,116 @@
 import typing
+import itertools
 import collections.abc
 
-from .. import contracts
-from .generic import success_shutdown_instance, failure_shutdown_instance
+from .. import contracts, exceptions
+from .generic import shutdown
 from .provider import DependencyProvider
 
 
-def _auto_detect_contract(
-    instance: contracts.DependencyBindingInstance,
-    contract: contracts.DependencyBindingContract | None
-) -> contracts.DependencyBindingContract | None:
-    if contract:
-        return contract
-    parent = None
-    for cls in instance.__mro__:
-        if cls == typing.Protocol:
-            return parent
-        parent = cls
-
-
-class BindingChain(contracts.DependencyBindingChain):
-    def __init__(self, dependency_registry: contracts.DependencyBingingRegistry):
-        self._dependency_registry = dependency_registry
-
-    def filter(self, clause: contracts.DependencyBindingChainFilterClause) -> 'BindingChain':
-        self._dependency_registry = {
-            contract: record
-            for contract, record in self._dependency_registry.items()
-            if clause(contract, record)
-        }
-        return self
-
-    def map(
-        self,
-        key_clause: contracts.DependencyBindingChainMapClause,
-        value_clause: contracts.DependencyBindingChainMapClause
-    ) -> 'BindingChain':
-        self._dependency_registry = {
-            key_clause(contract, record): value_clause(contract, record)
-            for contract, record in self._dependency_registry.items()
-        }
-        return self
-
-    def items(self) -> contracts.DependencyBingingRegistry:
-        return self._dependency_registry
-
-    def values(self) -> collections.abc.Iterable[contracts.DependencyBindingRecord]:
-        return self._dependency_registry.values()
-
-    def keys(self) -> collections.abc.Iterable[contracts.DependencyBindingContract]:
-        return self._dependency_registry.keys()
-
-
 class DependencyContainer(contracts.DependencyContainer):
-    def __init__(self):
-        self._dependency_registry = {}
-        self._callbacks = {}
-        self._instance_registry = {}
-
-    def register_dependency(
+    def __init__(
         self,
-        instance: contracts.DependencyBindingInstance,
-        contract: contracts.DependencyBindingContract,
-        binding_type: contracts.DependencyBindingType,
-        scope: contracts.DependencyBindingScope = contracts.DependencyBindingScope.GLOBAL
-    ) -> typing.NoReturn:
-        contract = _auto_detect_contract(instance, contract)
-        if contract:
-            self._dependency_registry[contract] = contracts.DependencyBindingRecord(binding_type, instance, scope)
-
-    def register_contractual_dependency(
-        self,
-        instance: contracts.DependencyBindingInstance,
-        contract: contracts.DependencyBindingContract | None = None
-    ) -> typing.NoReturn:
-        contract = _auto_detect_contract(instance, contract)
-        self.register_dependency(
-            instance,
-            contract,
-            contracts.DependencyBindingType.CONTRACTUAL
-        )
-
-    def register_selfish_dependency(
-        self,
-        instance: contracts.DependencyBindingInstance,
-        singleton_scope: contracts.DependencyBindingScope | None = None
-    ) -> typing.NoReturn:
-        binding_type = contracts.DependencyBindingType.SELFISH
-        scope = contracts.DependencyBindingScope.GLOBAL
-
-        if singleton_scope:
-            binding_type = contracts.DependencyBindingType.SINGLETON
-            scope = singleton_scope
-
-        self.register_dependency(
-            instance,
-            instance,
-            binding_type,
-            scope
-        )
-
-    def register_global_singleton(
-        self,
-        instance: contracts.DependencyBindingInstance,
-        contract: contracts.DependencyBindingContract | None = None,
-    ) -> typing.NoReturn:
-        self.register_dependency(
-            instance,
-            contract,
-            contracts.DependencyBindingType.SINGLETON
-        )
-
-    def register_instance_singleton(
-        self,
-        instance: contracts.DependencyBindingInstance,
-        contract: contracts.DependencyBindingContract | None = None,
-    ) -> typing.NoReturn:
-        self.register_dependency(
-            instance,
-            contract,
-            contracts.DependencyBindingType.SINGLETON,
-            contracts.DependencyBindingScope.INSTANCE
-        )
-
-    def register_callback(
-        self,
-        callback: typing.Callable,
-        contract: contracts.DependencyBindingContract,
-        event_type: contracts.EventType
-    ) -> typing.NoReturn:
-        self._callbacks[contract] = contracts.CallbackRecord(event_type, callback)
-
-    def register_shutdown_callback(
-        self,
-        success_callback: typing.Callable,
-        failure_callback: typing.Callable,
-        contract: contracts.DependencyBindingContract
+        bindings: collections.abc.Iterable[contracts.DependencyBindingRecord] | None = None
     ):
-        self._callbacks[(contract, contracts.EventType.ON_SHUTDOWN_SUCCESS)] = success_callback
-        self._callbacks[(contract, contracts.EventType.ON_SHUTDOWN_FAILURE)] = failure_callback
+        self._bindings = {}
+        self._callbacks = []
+        self._literals = {}
+        if bindings:
+            for record in bindings:
+                self.register(
+                    instance=record.instance,
+                    contract=record.contract,
+                    binding_type=record.binding_type,
+                    scope=record.scope
+                )
+        self._container_singleton_registry = {}
 
-    def get_bindings(self) -> BindingChain:
-        return BindingChain(self._dependency_registry)
+    def register(
+        self,
+        instance: contracts.DependencyBindingInstance,
+        contract: contracts.DependencyBindingContract | None = None,
+        binding_type: contracts.DependencyBindingType | None = contracts.DependencyBindingType.CONTRACTUAL,
+        scope: contracts.DependencyBindingScope | None = contracts.DependencyBindingScope.APPLICATION
+    ) -> typing.NoReturn:
+        if binding_type == contracts.DependencyBindingType.LITERAL:
+            if not isinstance(instance, typing.Mapping):
+                raise exceptions.LiteralMustBeMapping(instance=instance)
+            else:
+                self._literals |= instance
+
+        elif binding_type in (
+            contracts.DependencyBindingType.ON_STARTUP_CALLBACK,
+            contracts.DependencyBindingType.ON_SHUTDOWN_FAILURE_CALLBACK,
+            contracts.DependencyBindingType.ON_SHUTDOWN_SUCCESS_CALLBACK
+        ):
+            if not callable(instance):
+                raise exceptions.CallbackInstanceMustBeCallable(instance=instance, contract=contract)
+
+            self._callbacks.append(
+                contracts.DependencyBindingRecord(
+                    binding_type=binding_type,
+                    instance=instance,
+                    contract=contract,
+                    scope=scope
+                )
+            )
+        else:
+            contract = self._detect_contract(instance, contract)
+            if not contract:
+                raise exceptions.ContractNotDefined(instance=instance, contract=contract)
+
+            self._bindings[contract] = contracts.DependencyBindingRecord(
+                binding_type=binding_type,
+                instance=instance,
+                contract=contract,
+                scope=scope
+            )
+        self._integrity_check()
 
     def get_provider(self) -> DependencyProvider:
         return DependencyProvider(
-            bindings=self._dependency_registry,
+            bindings=self._bindings,
             callbacks=self._callbacks,
-            instance_registry=self._instance_registry,
+            literals=self._literals,
+            container_singleton_registry=self._container_singleton_registry
         )
 
-    async def shutdown(self, exc_type: typing.Any) -> typing.NoReturn:
-        for key, callback in self._callbacks.items():
-            _contract, _type = key
-            _desired = contracts.EventType.ON_SHUTDOWN_FAILURE if exc_type else contracts.EventType.ON_SHUTDOWN_SUCCESS
-            if _type == _desired:
-                singleton_instance = self._get_from_registry(_contract)
-                if exc_type:
-                    await failure_shutdown_instance(callback, singleton_instance, exc_type)
-                else:
-                    await success_shutdown_instance(callback, singleton_instance)
+    def get_registry(
+        self
+    ) -> collections.abc.Mapping[contracts.DependencyBindingContract, contracts.DependencyBindingRecord]:
+        return self._container_singleton_registry
 
-    def _get_from_registry(
-        self,
-        contract: contracts.DependencyBindingContract
-    ) -> contracts.DependencyBindingInstance:
-        return self._instance_registry.get(contract)
+    async def shutdown(self, exc_type: typing.Any) -> typing.NoReturn:
+        await shutdown(self, exc_type, contracts.DependencyBindingScope.APPLICATION)
+
+    @staticmethod
+    def _detect_contract(
+        instance: contracts.DependencyBindingInstance,
+        contract: contracts.DependencyBindingContract | None
+    ) -> contracts.DependencyBindingContract | None:
+        if contract:
+            return contract
+        parent = None
+        for cls in instance.__mro__:
+            if cls == typing.Protocol:
+                return parent
+            parent = cls
+
+    def _integrity_check(self):
+        for callback_record in self._callbacks:
+            binding_record = self._bindings.get(callback_record.contract)
+            if binding_record and binding_record.binding_type != contracts.DependencyBindingType.SINGLETON:
+                raise exceptions.CallbackCanBeAttachedToSingletonOnly(
+                    instance=callback_record.instance,
+                    contract=callback_record.contract
+                )
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.shutdown(exc_type)
+
+    def __iter__(self) -> typing.Generator[contracts.DependencyBindingRecord, None, None]:
+        for record in itertools.chain(self._bindings.values(), self._callbacks):
+            yield record
--- zorge/di/generic.py
+++ zorge/di/generic.py
@@ -4,26 +4,59 @@ import typing
 from .. import contracts
 
 
-async def success_shutdown_instance(
-    callback_function,
-    instance: contracts.SingletonInstance
-):
-    if not instance:
-        return
-    if inspect.iscoroutinefunction(callback_function):
-        await callback_function(instance)
-    elif callable(callback_function):
-        callback_function(instance)
+async def startup(
+    dependency_provider: contracts.DependencyProvider,
+) -> typing.NoReturn:
+    for record in dependency_provider:
+        if record.binding_type == contracts.DependencyBindingType.ON_STARTUP_CALLBACK:
+            instance = dependency_provider.get_registry().get(record.contract)
+            if not instance:
+                instance = await dependency_provider.get(record.contract)
+            if instance:
+                await _evoke_callback(
+                    callback_function=record.instance,
+                    instance=instance
+                )
 
 
-async def failure_shutdown_instance(
+async def shutdown(
+    dependency_space: contracts.DependencyProvider | contracts.DependencyContainer,
+    exc_type: typing.Any,
+    scope: contracts.DependencyBindingScope
+) -> typing.NoReturn:
+    _callback_types = {
+        True: contracts.DependencyBindingType.ON_SHUTDOWN_FAILURE_CALLBACK,
+        False: contracts.DependencyBindingType.ON_SHUTDOWN_SUCCESS_CALLBACK
+    }
+    for record in dependency_space:
+        if (
+            record.scope == scope
+            and
+            record.binding_type == _callback_types.get(bool(exc_type))
+        ):
+            params = dict(
+                callback_function=record.instance,
+                instance=dependency_space.get_registry().get(record.contract)
+            )
+            if bool(exc_type):
+                params['exc_type'] = exc_type
+            await _evoke_callback(**params)
+
+
+async def _evoke_callback(
     callback_function,
     instance: contracts.SingletonInstance,
-    exc_type: typing.Any
+    exc_type: typing.Any = contracts.NotDefined
 ):
     if not instance:
         return
     if inspect.iscoroutinefunction(callback_function):
-        await callback_function(instance, exc_type)
+        if exc_type is contracts.NotDefined:
+            await callback_function(instance)
+        else:
+            await callback_function(instance, exc_type)
     elif callable(callback_function):
-        callback_function(instance, exc_type)
+        if exc_type is contracts.NotDefined:
+            callback_function(instance)
+        else:
+            callback_function(instance, exc_type)
